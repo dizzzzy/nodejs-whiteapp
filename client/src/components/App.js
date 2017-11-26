d8cfc22f62764337658b1f81eef6aa18cc06c4e5
James <dimitriosziavras@hotmail.com>
2017-11-26 02:06:31 -0500

    Removed react template/added redux and react-router packages

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = function(){
+    return(
+        <div>
+            Hi There!
+            <a href="/auth/google/"> Sign In With Google</a>
+        </div>
+    );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+const store = createStore(function(){
+        return [];
+    },
+    {},
+    applyMiddleware()
+);
+
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>,
+    document.getElementById('root')
+);
