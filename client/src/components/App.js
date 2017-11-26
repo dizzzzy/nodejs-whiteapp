db9755dc66ab04c9158e82711919ef83da25a66e
James <dimitriosziavras@hotmail.com>
2017-11-26 05:20:39 -0500

    Initial navbar setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header'
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -13,7 +13,10 @@ const App = function(){
             {/*<a href="/auth/google/"> Sign In With Google</a>*/}
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing}/>
+                    <Header/>
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/surveys" component={Dashboard}/>
+                    <Route exact path="/surveys/new" component={SurveyNew}/>
                 </div>
             </BrowserRouter>
         </div>
