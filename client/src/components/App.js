97b01844f1f0cb0812d48ae305504751db1cfe79
James <dimitriosziavras@hotmail.com>
2017-11-26 02:43:23 -0500

    Added dummy authentication reducer/Started routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = function(){
     return(
         <div>
-            Hi There!
-            <a href="/auth/google/"> Sign In With Google</a>
+            {/*Hi There!*/}
+            {/*<a href="/auth/google/"> Sign In With Google</a>*/}
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={Landing}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 const store = createStore(function(){
-        return [];
+        return reducers;
     },
     {},
     applyMiddleware()
