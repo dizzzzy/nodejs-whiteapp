0f89047fdf3c14379799c0f9e62cabaae9fddd4d
James <dimitriosziavras@hotmail.com>
2017-11-27 01:19:01 -0500

    Refactored fethcUser action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,12 +2,11 @@ import axios from 'axios'
 import { FETCH_USER } from "./types";
 
 export const fetchUser = function(){
-    return function(dispatch){ //when you return a function for your action then we get access to the dispatch function
-        axios.get('/api/current_user').then(function(res){
-            dispatch({
-                type: FETCH_USER,
-                payload: res
-            });
+    return async function(dispatch){ //when you return a function for your action then we get access to the dispatch function
+        const res = await axios.get('/api/current_user');
+        dispatch({
+            type: FETCH_USER,
+            payload: res
         });
     };
-};   
+};
