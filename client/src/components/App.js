4b4aba986c0a4a15b08765680a11906af58b71e2
James <dimitriosziavras@hotmail.com>
2017-11-29 21:39:35 -0500

    Added Search Bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import Header from './Header';
 import Landing from "./Landing";
 import ChipExampleArray from "./ChipExampleArray";
 import TagsExample from "./TagsExample"
-//import YoutubeSearch from "./Youtube"
+import YoutubeSearch from "./Youtube"
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Profile = () => <h2>Profile</h2>;
@@ -25,7 +25,7 @@ class App extends Component{
                 <BrowserRouter>
                     <div>
                         <Header/>
-                        <Route exact path="/" component={Landing}/>
+                        <Route exact path="/" component={YoutubeSearch}/>
                         <Route exact path="/classes" component={Dashboard}/>
                         <Route exact path="/videos/new" component={SurveyNew}/>
                         <Route exact path="/profile" component={TagsExample}/>
--- client/src/components/Youtube.js
+++ client/src/components/Youtube.js
@@ -1,21 +1,40 @@
-// import React, { Component } from 'react'
-// import YouTubeAutocomplete from 'react-youtube-autocomplete';
-//
-// export default class YoutubeSearch extends Component{
-//
-//     render() {
-//         return (
-//             <YouTubeAutocomplete
-//                 apiKey=""
-//                 placeHolder="Search Youtube"
-//                 maxResults="20"
-//                 callback={this._onSearchResultsFound}
-//             />
-//         );
-//     }
-//
-//     _onSearchResultsFound(results) {
-//         // Results is an array of retreived search results from Youtube.
-//         // Do what you want with the results here.
-//     }
-// }
+import React, { Component } from 'react'
+import YouTubeAutocomplete from 'react-youtube-autocomplete';
+
+export default class YoutubeSearch extends Component{
+
+    render() {
+        return (
+            <YouTubeAutocomplete
+                apiKey="AIzaSyBVHcVyoGwt_VbwbE2noVwBz0kPwkT-rGc"
+                placeHolder="Search Youtube"
+                maxResults="20"
+                callback={this._onSearchResultsFound}
+            />
+        );
+    }
+
+    _onSearchResultsFound(results) {
+        var lis= [];
+        for(var i=0; i<19; i++){
+            lis.push(
+                <div key={i}>
+                    <h2>{results[i].snippet.title}</h2>
+                    <iframe className='video w100'
+                            width="640px"
+                            height="360px"
+                            src="//www.youtube.com/embed/{results[i].id.videoId}"
+                            frameBorder="0"
+                            allowFullScreen>
+                    </iframe>
+                </div>);
+        }
+        const data= (
+            <div>
+                {lis}
+            </div>
+        );
+        console.log(data);
+        return(data);
+    }
+}
